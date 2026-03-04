c6f58924d7295ca9b4ce02657b6e01ee0fb49124
veeksharai0510 <veeksharai0501@gmail.com>
2026-03-04 22:04:29 +0530

    Update test.py

--- sonarqube/test.py
+++ sonarqube/test.py
@@ -1,1 +1,14 @@
+def _normalize_raw_value(raw_value):
+    """Normalize raw_value to always be a list."""
+    if isinstance(raw_value, dict):
+        return [raw_value]
+    elif not isinstance(raw_value, list):
+        return [raw_value]
+    return raw_value
 
+
+def _extract_attr_values(sub_attr):
+    """Extract attribute name-value pairs from sub_attr."""
+    vals = []
+    raw_value = sub_attr.get("value", [])
+    raw_value = _normalize_raw_value(raw_value)
